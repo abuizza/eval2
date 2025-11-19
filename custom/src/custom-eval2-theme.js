914c1d29806ddf9b907fb8f9fc84c03443b69b6b
Abu Izza Ahmad <abuizzaahmad@gmail.com>
2025-11-19 17:57:10 +0700

    'Page details updated: Welcome (item-9df2478a-800b-44fd-94b2-ca8018ecc084)'

--- custom/src/custom-eval2-theme.js
+++ custom/src/custom-eval2-theme.js
@@ -2,7 +2,15 @@
  * Copyright 2025 andyprodistik
  * @license Apache-2.0, see License.md for full text.
  */
-import { HAXCMSLitElementTheme, css, unsafeCSS, html, store, autorun, toJS } from "@haxtheweb/haxcms-elements/lib/core/HAXCMSLitElementTheme.js";
+import {
+  HAXCMSLitElementTheme,
+  css,
+  unsafeCSS,
+  html,
+  store,
+  autorun,
+  toJS,
+} from "@haxtheweb/haxcms-elements/lib/core/HAXCMSLitElementTheme.js";
 import { PolarisFlexTheme } from "@haxtheweb/polaris-theme/lib/polaris-flex-theme.js";
 import "@haxtheweb/haxcms-elements/lib/ui-components/blocks/site-children-block.js";
 
@@ -30,17 +38,22 @@ class CustomEval2Theme extends PolarisFlexTheme {
       css`
         :host {
           display: block;
+          font-family: "Mona Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
+          font-size: 20px;
+          font-weight: 400;
+          line-height: 32px;
         }
-        
+
         /* Desktop and tablet layout */
         aside {
           float: left;
           width: 240px;
         }
-        
+
         aside section h4 {
           font-size: 16px;
-          margin: var(--ddd-spacing-0) var(--ddd-spacing-0) var(--ddd-spacing-6) var(--ddd-spacing-0);
+          margin: var(--ddd-spacing-0) var(--ddd-spacing-0) var(--ddd-spacing-6)
+            var(--ddd-spacing-0);
           text-transform: lowercase;
           font-family: "Open Sans", sans-serif;
           font-weight: 300;
@@ -62,7 +75,7 @@ class CustomEval2Theme extends PolarisFlexTheme {
           font-family: "Roboto Condensed", sans-serif;
           font-size: 16px;
         }
-        
+
         /* Mobile responsive layout */
         @media (max-width: 768px) {
           aside {
@@ -70,16 +83,16 @@ class CustomEval2Theme extends PolarisFlexTheme {
             width: 100%;
             clear: both;
           }
-          
+
           aside[role="complementary"] {
             display: none; /* Sembunyikan sidebar di layar kecil */
           }
-          
+
           aside section {
             margin-bottom: var(--ddd-spacing-5);
           }
         }
-        
+
         @media (min-width: 769px) {
           aside[role="complementary"] {
             display: block; /* Tampilkan sidebar di layar besar */
@@ -90,40 +103,68 @@ class CustomEval2Theme extends PolarisFlexTheme {
   }
 
   /**
-   * Overload methods for customization of slots from the base class 
+   * Overload methods for customization of slots from the base class
    */
   renderHeaderSlot() {
-    return html``
+    return html``;
   }
 
   renderSideBar() {
     return html`
-    <aside
-          role="complementary"
-          aria-label="Primary Sidebar"
-          itemtype="http://schema.org/WPSideBar"
-          part="page-primary-sidebar"
-        >
-          <section>
-            <site-children-block
-              part="page-children-block"
-              dynamic-methodology="ancestor"
-            ></site-children-block>
-          </section>
-        </aside>
-    `
+      <aside
+        role="complementary"
+        aria-label="Primary Sidebar"
+        itemtype="http://schema.org/WPSideBar"
+        part="page-primary-sidebar"
+      >
+        <section>
+          <site-children-block
+            part="page-children-block"
+            dynamic-methodology="ancestor"
+          ></site-children-block>
+        </section>
+      </aside>
+      <article itemscope itemtype="http://schema.org/Article">
+        <header>
+          <h1 itemprop="headline">Sharp looking section</h1>
+          <meta itemprop="author" content="Author Name" />
+          <meta itemprop="datePublished" content="2025-01-01" />
+          <meta itemprop="dateModified" content="2025-01-02" />
+          <meta
+            itemprop="image"
+            content="files/syahrul-alamsyah-wahid-Z1NnYaPD6Vk-unsplash.jpg"
+          />
+        </header>
+        <section itemprop="articleBody">
+          <p>Kelihatan mudah dengan belajar bersama.</p>
+          <simple-cta
+            hotline
+            filled=""
+            outlined=""
+            slot="buttons"
+            accent-color="grey"
+            remote-link-target="{}"
+            icon="communication:call-missed-outgoing"
+            link="/halo"
+            label="Buka"
+            remote-link-url="/halo"
+          >
+          </simple-cta>
+        </section>
+      </article>
+    `;
   }
 
   renderFooterContactInformation() {
-    return html``
+    return html``;
   }
 
   renderFooterSecondarySlot() {
-    return html``
+    return html``;
   }
 
   renderFooterPrimarySlot() {
-    return html``
+    return html``;
   }
 
   /**
@@ -133,7 +174,7 @@ class CustomEval2Theme extends PolarisFlexTheme {
   static get tag() {
     return "custom-eval2-theme";
   }
- 
+
   constructor() {
     super();
   }
